5449a37a21effc8a7d8206f5e69f4f4504c778b1
Abdul-Rahman258 <emailabdulrahmanbaig@gmail.com>
2026-08-01 14:46:32 +0500

    Revert sphere animation to move left (-35vw) at the end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
   const heroY = useTransform(scrollYProgress, [0, 1], ["0%", "50%"]);
 
   // Sphere transformations
-  const sphereX = useTransform(scrollYProgress, [0, 0.3, 0.55, 0.9, 1], ["25vw", "25vw", "0vw", "0vw", "35vw"]);
+  const sphereX = useTransform(scrollYProgress, [0, 0.3, 0.55, 0.9, 1], ["25vw", "25vw", "0vw", "0vw", "-35vw"]);
   const sphereScale = useTransform(scrollYProgress, [0, 0.3, 0.55, 0.9, 1], [1, 1, 5, 5, 0.8]);
   const sphereGlowOpacity = useTransform(scrollYProgress, [0, 0.55, 0.9, 1], [0, 0, 1, 1]);
 
